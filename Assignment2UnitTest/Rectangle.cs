627f00771b77f94a70f114bdd7403a25b17c3ed6
Rishik Bakshi <Rbakshi3343@conestogac.on.ca>
2022-02-11 22:59:24 -0500

    the completed Rectangle class

---
+++ Assignment2UnitTest/Rectangle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2UnitTest
+{
+    class Rectangle
+    {
+        private int length;
+        private int width;
+        public Rectangle()
+        {
+            length = 1;
+            width = 1;
+        }
+        public Rectangle(int length, int width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public int GetLength()
+        {
+            return length;
+        }
+        public int SetLength(int length)
+        {
+            this.length = length;
+            return length;
+        }
+        public int GetWidth()
+        {
+            return width;
+        }
+        public int SetWidth(int width)
+        {
+            this.width = width;
+            return width;
+        }
+        public int GetPerimeter()
+        {
+            int peri = 2 * (length + width);
+            return (peri);
+        }
+        public int GetArea()
+        {
+            int area = (length * width);
+            return (area);
+        }
+    }
+}
